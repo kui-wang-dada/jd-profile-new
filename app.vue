a810a5c07b602d63f0b4835761647f24228140b2
kui <872505550@qq.com>
2023-05-22 07:56:40 +0800

    init

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted, computed } from 'vue'
+import { useRouter } from 'vue-router'
+const props = defineProps({})
+onMounted(() => { })
+let queryParams = computed(() => { })
+</script>
+<template>
+  <div class='wrap'>
+    <CommonTopBar />
+    <NuxtLayout>
+      <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+      <NuxtPage />
+    </NuxtLayout>
+    <CommonFooter />
+  </div>
+</template>
+<style scoped lang='less'></style>
---
+++ components/common/TopBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref, onMounted, computed } from 'vue'
+import { useRouter } from 'vue-router'
+const props = defineProps({})
+onMounted(() => { })
+let queryParams = computed(() => { })
+</script>
+<template>
+  <div class='wrap'>
+    头部
+  </div>
+</template>
+<style scoped lang='less'></style>
